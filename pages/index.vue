1c2155189f7d6661205e1e6f980e556f267b0ec4
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2023-12-26 15:37:22 +0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+
+<body class="HomePage">
+<h1 class="H1HomePage">My Home</h1>
+
+<div class="card-containerHomePage">
+<NuxtLink to="/todo" class="cardHomePage">Todo Lists</NuxtLink>
+<NuxtLink to="/memorizeCard" class="cardHomePage">Memory Game</NuxtLink>
+<NuxtLink to="/wordGuess" class="cardHomePage">Word Game</NuxtLink>
+</div>
+</body>
+</template>
+
+
+
---
+++ pages/todo.vue
@@ -0,0 +1,79 @@
+<template>
+<body class="TodoList">
+<div class="containerTodoList">
+    <div id="myDIVTodoList" class="headerTodoList">
+        <h2>Tasks list</h2>
+        <input type="text" id="myInputTodoList" placeholder="Add your Task">
+        <span onclick="newElement()" class="addBtnTodoList">Add</span>
+    </div>
+
+    <ul id="myULTodoList">
+        <li>Hit the gym</li>
+        <li class="checked">Pay bills</li>
+        <li>Meet George</li>
+        <li>Buy eggs</li>
+        <li>Read a book</li>
+        <li>Organize office</li>
+    </ul>
+</div>
+</body>
+</template>
+
+<script setup lang="ts">
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInputTodoList").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("myULTodoList").appendChild(li);
+    }
+    document.getElementById("myInputTodoList").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+</script>
